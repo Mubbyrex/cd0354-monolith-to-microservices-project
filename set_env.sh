d1a389f40a47cce13ff1cbd7f575aff08a1aaaf8
mubarak <mubbyre6xx@gmail.com>
2022-09-29 17:32:59 +0100

    added a reverseproxy

--- set_env.sh
+++ set_env.sh
@@ -1,11 +1,10 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=mubarakdb
+export POSTGRES_USERNAME=postgres
 export POSTGRES_PASSWORD=Temmyre7x
-export POSTGRES_HOST=mubarakdb.c1z08ey8ykpq.us-east-1.rds.amazonaws.com
+export POSTGRES_HOST=postgres.cdjlyj08pkfu.us-east-2.rds.amazonaws.com
 export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::my-091294840794-bucket
-export AWS_REGION=us-east-1
+export AWS_BUCKET=arn:aws:s3:::my-467286114294-bucket
 export AWS_PROFILE=default
 export JWT_SECRET=testing
 export URL=http://localhost:8100
