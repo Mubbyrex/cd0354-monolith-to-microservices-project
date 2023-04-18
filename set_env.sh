1fe25da0d3d74585963a23a5557c6f6c18e7f3db
Mubarak <mubbyre6xx@gmail.com>
2023-04-18 17:49:12 +0100

    Update set_env.sh

--- set_env.sh
+++ set_env.sh
@@ -1,7 +1,7 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=Temmyre7x
+export POSTGRES_PASSWORD=
 export POSTGRES_HOST=postgres.cdjlyj08pkfu.us-east-2.rds.amazonaws.com
 export POSTGRES_DB=postgres
 export AWS_BUCKET=arn:aws:s3:::my-467286114294-bucket
